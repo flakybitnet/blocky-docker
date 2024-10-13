37dcd644ae1373cb1214ac338c254f209038f911
0x1def <0x1def@flakybit.net>
2024-10-13 21:11:14 +0300

    Added CI

---
+++ .ci/get-dist.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -eu
+
+set -a
+. .ci/lib.sh
+set +a
+
+echo "Update and install required system packages and dependencies"
+apt update
+apt install -y wget
+
+echo "Downloading blocky $BLOCKY_VERSION"
+wget "https://github.com/0xERR0R/blocky/releases/download/$BLOCKY_VERSION/blocky_${BLOCKY_VERSION}_Linux_x86_64.tar.gz"
+wget "https://github.com/0xERR0R/blocky/releases/download/$BLOCKY_VERSION/blocky_checksums.txt"
+sha256sum -c --ignore-missing blocky_checksums.txt
+mkdir dist
+tar -xpzf "blocky_${BLOCKY_VERSION}_Linux_x86_64.tar.gz" -C dist
+
+echo "Downloading AdGuard block lists"
+wget https://github.com/zc-devs/agh2blocky/archive/refs/heads/main.tar.gz -O agh-lists.tar.gz
+mkdir -p blocklists/agh
+tar -xpzf agh-lists.tar.gz -C blocklists/agh --strip-components=2 agh2blocky-main/filters
+
+echo 'Done'
+
